ceadc1b78feca7b20ec5b2203edc45de64a246ad
Kard0305 <kard030305@gmail.com>
2024-06-21 10:03:38 -0300

    Add files via upload

---
+++ crm.sql
@@ -0,0 +1,18 @@
+create database crm_db;
+ use crm_db; 
+ 
+ create table clientes (
+ id_clientes  int auto_increment not null  primary key,
+ nome varchar (90) not null,
+ telefone decimal (12,4) not null,
+ email varchar (100) not null );
+ 
+ 
+create table usuario(
+id_usuario int auto_increment not null primary key,
+nome varchar (90) not null,
+telefone decimal (12,4) not null,
+email varchar (100) not null,
+senha decimal (12,2) not null);
+
+select *from  clientes 
